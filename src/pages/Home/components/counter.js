e88c2812d2876a14c5a32878b4795efd4bf8f2c6
poojajain48 <poojajain.mobiwebtech@gmail.com>
2021-02-16 19:24:54 +0530

    fix home page design

--- src/pages/Home/components/counter.js
+++ src/pages/Home/components/counter.js
@@ -25,18 +25,9 @@ export const Counter = props => {
         className="col-4 text-center"
         style={{ position: "absolute", right: 4, top: -34 }}
       >
-        <div
-          style={{
-            marginLeft: 50,
-            marginRight: 50,
-            borderBottom: "4px solid white",
-            borderLeft: "4px solid white",
-            borderRight: "4px solid white",
-            position: "relative",
-            top: 74,
-            height: 250,
-          }}
-        />
+        <div className="borderPipe"><span></span> <span></span></div>
+          
+        
         <div className="numbers">
           <div style={{ top: 10 }}>
             {
